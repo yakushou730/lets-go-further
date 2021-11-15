ace2c0168acac74514d4b96d1cd79f05a4ae3757
Shou Tseng <yakushou730@gmail.com>
2021-11-15 20:53:29 +0800

    feat: setting up the movie model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/yakushou730/lets-go-further/internal/data"
+
 	_ "github.com/lib/pq"
 )
 
@@ -31,6 +33,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -58,6 +61,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
