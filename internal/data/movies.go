b616ba9d26912561277817a29b754a36e493f530
Shou Tseng <yakushou730@gmail.com>
2021-11-16 16:30:20 +0800

    feat: deleting a movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -108,5 +108,27 @@ RETURNING version`
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM movies
+		WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
