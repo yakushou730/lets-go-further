de1e5464b4e276a38e7a2729b7bc3ab59d33caf4
Shou Tseng <yakushou730@gmail.com>
2021-11-20 09:35:14 +0800

    feat: returning pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,10 +1,32 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"github.com/yakushou730/lets-go-further/internal/validator"
 )
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
+}
 
 type Filters struct {
 	Page         int
