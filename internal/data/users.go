ffcc2176cd58d3c698b823fa5deba0278731fd46
Shou Tseng <yakushou730@gmail.com>
2021-11-30 20:47:20 +0800

    feat: authenticating requests

--- internal/data/users.go
+++ internal/data/users.go
@@ -14,6 +14,7 @@ import (
 
 var (
 	ErrDuplicateEmail = errors.New("duplicate email")
+	AnonymousUser     = &User{}
 )
 
 type User struct {
@@ -24,6 +25,10 @@ type User struct {
 	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 type password struct {
