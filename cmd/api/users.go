7b2e5e106fdef238ba2a347381634a6709d31b04
Shou Tseng <yakushou730@gmail.com>
2021-11-30 15:27:17 +0800

    feat: sending activation tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/yakushou730/lets-go-further/internal/validator"
 
@@ -53,8 +54,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		userData := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", userData)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
