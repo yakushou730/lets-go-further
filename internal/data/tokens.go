ee619caa4ec93c19c40076e983d6d0d377829e7e
Shou Tseng <yakushou730@gmail.com>
2021-11-30 22:42:02 +0800

    feat: requiring user activation

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -2,10 +2,10 @@ package data
 
 import (
 	"context"
+	"crypto/rand"
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base32"
-	"math/rand"
 	"time"
 
 	"github.com/yakushou730/lets-go-further/internal/validator"
