cb50e5be1dfc9b44958dc2b80791dbb76d9acf84
Shou Tseng <yakushou730@gmail.com>
2021-12-05 01:32:29 +0800

    feat: simple cors requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -42,6 +43,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -72,6 +76,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "9bc755430b759a", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "67157ded65f539", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.yakushou.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
