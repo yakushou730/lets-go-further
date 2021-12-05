8e4397787d917102c57f966bfde92d6e3b70f326
Shou Tseng <yakushou730@gmail.com>
2021-12-05 20:44:00 +0800

    feat: recording http status codes

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/felixge/httpsnoop"
 
 	"github.com/yakushou730/lets-go-further/internal/validator"
 
@@ -211,15 +214,15 @@ func (app *application) metrics(next http.Handler) http.Handler {
 	totalRequestsReceived := expvar.NewInt("total_requests_received")
 	totalResponsesSent := expvar.NewInt("total_responses_sent")
 	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_ms")
+	totalResponsesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
 		totalRequestsReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
 
 		totalResponsesSent.Add(1)
-		duration := time.Since(start).Microseconds()
-		totalProcessingTimeMicroseconds.Add(duration)
+		totalProcessingTimeMicroseconds.Add(metrics.Duration.Microseconds())
+		totalResponsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
 	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yakushou730/lets-go-further
 go 1.17
 
 require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
